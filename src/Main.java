b4811912b53b7c43cde7cf4e9a216051af13e767
Raptureforlife <dklartey004@st.ug.edu.gh>
2026-06-17 11:14:19 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int size = input.nextInt();
+
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = input.nextInt();
+        }
+
+        System.out.print("Enter target value: ");
+        int target = input.nextInt();
+
+        int linearResult = Algorithms.linearSearch(array, target);
+
+        Arrays.sort(array);
+
+        int binaryResult = Algorithms.binarySearch(array, target);
+
+        System.out.println("\nLinear Search Result:");
+        if (linearResult != -1) {
+            System.out.println("Target found at index " + linearResult);
+        } else {
+            System.out.println("Target not found");
+        }
+
+        System.out.println("\nBinary Search Result:");
+        if (binaryResult != -1) {
+            System.out.println("Target found at index " + binaryResult);
+        } else {
+            System.out.println("Target not found");
+        }
+
+        input.close();
     }
 }
